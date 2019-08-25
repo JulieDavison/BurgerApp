7b09e7ab324dadffc80dc702f57cf00402ee8916
Julie Davison <davison.jules@gmail.com>
2019-08-25 16:27:40 -0700

    complete file setup

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burgername, devoured) VALUES ('Quac Burger', 0);
+INSERT INTO burgers (burgername, devoured) VALUES ('BBQ Burger', 0);
+INSERT INTO burgers (burgername, devoured) VALUES ('Cheeseburger', 1);
